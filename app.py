49313f82ef9ab3c837fe0b2bad4652533b1dc251
sorapong <33795437+serversuck@users.noreply.github.com>
2024-05-22 06:50:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,44 +12,33 @@ st.write("Upload your Image...")
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/last.pt', force_reload=True)
 
-uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
-if uploaded_file is not None:
-    file_bytes = np.asarray(bytearray(uploaded_file.read()))
-    image = cv2.imdecode(file_bytes, 1)
+def detect_objects(frame):
+    # Convert frame to a suitable format for YOLOv5
+    img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    results = model(img, size=640)  # Reduce size if needed
+    return results
+def main():
+    run = st.checkbox('Run')
+    stframe = st.empty()
+    video_capture = cv2.VideoCapture(0)
 
-    imgRGB = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
-
-    st.write("")
-    st.write("Detecting...")
-    result = model(imgRGB, size=600)
-  
-    detect_class = result.pandas().xyxy[0]
-  
-    st.code(detect_class[['name', 'xmin','ymin', 'xmax', 'ymax']])
-  
-    # Count each class
-    class_counts = detect_class['name'].value_counts().to_dict()
-    st.write("Class counts:")
+    while run:
+        ret, frame = video_capture.read()
+        if not ret:
+            st.write("Failed to capture image")
+            break
+        
+        results = detect_objects(frame)
+        results.render()  # Updates results.imgs with boxes and labels
+        
+        # Convert the frame with detections to an image format for streamlit
+        img = Image.fromarray(cv2.cvtColor(results.imgs[0], cv2.COLOR_BGR2RGB))
+        stframe.image(np.array(img), channels="RGB")
     
-    for class_name, count in class_counts.items():
-        st.write(f"- **{class_name.capitalize()}**: {count}")
+    video_capture.release()
 
-    
-  
-    outputpath = 'output.jpg'
-  
-    result.render()  # render bbox in image
-    for im in result.ims:
-        im_base64 = Image.fromarray(im)
-        im_base64.save(outputpath)
-        img_ = Image.open(outputpath)
-        st.image(img_, caption='Model Prediction(s)')
-    
-    for idx, row in detect_class.iterrows():
-        xmin, ymin, xmax, ymax, name = int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax']), row['name']
-        cropped_image = imgRGB[ymin:ymax, xmin:xmax]
-        st.image(cropped_image, caption=f"{name.capitalize()}" ,width=300)
-
+if __name__ == "__main__":
+    main()
   
 
 
